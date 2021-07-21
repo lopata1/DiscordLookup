72ccd8a40be8fde2b1e473b9ac00873a80c520da
Andrej Dusanic <Sarma6>
2021-07-21 14:23:43 +0200

    first commit

---
+++ getuserinfo.js
@@ -0,0 +1,52 @@
+const https = require('https')
+const id = process.argv.slice(2)[0]
+
+
+async function getData(id) { 
+	const gd = () => {
+		return new Promise((resolve, reject) => {
+			const options = {
+				hostname: 'discord.com',
+				port: 443,
+				path: '/api/v8/users/' + id,
+				headers: {'Authorization': 'Bot ODYyMzEyODE5NTQ2NjUyNzQy.YOWhZQ.VLklG8D8AU7-PhJCgv49hB7yKIQ'},
+				method: 'GET'
+			}
+
+			const req = https.request(options, (res) => {
+				console.log(`statusCode: ${res.statusCode}`)
+
+				
+				  res.on('data', d => {
+					  try {
+						  resolve(JSON.parse(d))
+					  }
+					  catch {
+						  console.log("ERROR - This user has non ascii username.")
+						  var nothing = {}
+						  resolve(nothing)
+					  }
+				  })
+				})
+
+				req.on('error', error => {
+				  console.error(error)
+				})
+			req.end()
+
+		})
+	}
+	return await gd()
+}
+
+const Snowflake = require("./discord/Snowflake")
+
+async function main() {
+	//console.log(creation
+	var creation = new Date(Snowflake.deconstruct(id).timestamp)
+	var data = await getData(id)
+	var userdata = "ID: "+ data.id + "\nUsername: " + data.username + "\nAvatar: " + data.avatar + "\nDiscriminator: " + data.discriminator + "\nPublic Flags: " + data.public_flags + "\nDate of Creation: " + creation
+
+	console.log(userdata)
+}
+main()
